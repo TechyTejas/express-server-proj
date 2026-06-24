45939a1441720dc90d0d5d58f0a6119b7a504f44
tejas <sadadetej.27@gmail.com>
2026-06-24 23:57:07 +0530

    sequelize implementation

--- students-app/server.js
+++ students-app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = 4000;
 const app = express();
+const db = require('./static/js/utils/db-connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 const studentRoutes = require('./routes/studentRoutes');
 const busRoutes = require('./routes/busRoutes');
 const studentDetailsRoutes = require('./routes/studentDetailsRoutes');
+
+//models
+const StudentDetails = require('./models/student-details');
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -19,6 +23,10 @@ app.use('/students', studentRoutes);
 app.use('/users', busRoutes);
 app.use('/student-details', studentDetailsRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+db.sync({force: false}).then(()=>{
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch((err)=>{
+  console.log(err);
 });
--- students-app/static/js/utils/db-connection.js
+++ students-app/static/js/utils/db-connection.js
@@ -1,77 +1,92 @@
-const mysql = require('mysql2');
+const {Sequelize} = require('sequelize');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Pass@2026",
-    database: "testdb",
-  });
+const sequelize = new Sequelize('testdb', 'root', 'Pass@2026', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+module.exports = sequelize;
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "Pass@2026",
+//     database: "testdb",
+//   });
   
-  connection.connect((err) => {
-    if (err) {
-      console.error("Error connecting to MySQL:", err.message);
-      return;
-    }
-    console.log("Connected to MySQL as id " + connection.threadId);
+//   connection.connect((err) => {
+//     if (err) {
+//       console.error("Error connecting to MySQL:", err.message);
+//       return;
+//     }
+//     console.log("Connected to MySQL as id " + connection.threadId);
   
-    const createTableQuery = `
-      create table IF NOT EXISTS Students(
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(20),
-      email VARCHAR(20)
-      )`
+//     const createTableQuery = `
+//       create table IF NOT EXISTS Students(
+//       id INT AUTO_INCREMENT PRIMARY KEY,
+//       name VARCHAR(20),
+//       email VARCHAR(20)
+//       )`
     
-    const createUsersTableQuery = `
-     create table IF NOT EXISTS Users(
-     id INT AUTO_INCREMENT PRIMARY KEY,
-     name VARCHAR(20),
-     email VARCHAR(20)
-     )`
+//     const createUsersTableQuery = `
+//      create table IF NOT EXISTS Users(
+//      id INT AUTO_INCREMENT PRIMARY KEY,
+//      name VARCHAR(20),
+//      email VARCHAR(20)
+//      )`
     
-    const createBusTableQuery = `
-     create table IF NOT EXISTS Buses(
-     id INT AUTO_INCREMENT PRIMARY KEY,
-     name varchar(20),
-     busNo varchar(20) UNIQUE NOT NULL,
-     totalSeats INT,
-     availableSeats INT
-     )`
+//     const createBusTableQuery = `
+//      create table IF NOT EXISTS Buses(
+//      id INT AUTO_INCREMENT PRIMARY KEY,
+//      name varchar(20),
+//      busNo varchar(20) UNIQUE NOT NULL,
+//      totalSeats INT,
+//      availableSeats INT
+//      )`
 
-    const createStudentTableQuery = `
-    create table IF NOT EXISTS StudentDetails(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20),
-    email VARCHAR(20) UNIQUE NOT NULL,
-    age INT NOT NULL)`
+//     const createStudentTableQuery = `
+//     create table IF NOT EXISTS StudentDetails(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(20),
+//     email VARCHAR(20) UNIQUE NOT NULL,
+//     age INT NOT NULL)`
 
-    // const createBusTableQuery = `
-    // create table Bus(
-    //   id INT AUTO_INCREMENT PRIMARY KEY,
-    //   bus_number INT,
-    //   total_seats INT,
-    //   available_seats INT
-    //   )`
-    // const createBookingTableQuery = `
-    // create table Booking(
-    //   id INT AUTO_INCREMENT PRIMARY KEY,
-    //   seat_number INT,
-    //   )`
+//     // const createBusTableQuery = `
+//     // create table Bus(
+//     //   id INT AUTO_INCREMENT PRIMARY KEY,
+//     //   bus_number INT,
+//     //   total_seats INT,
+//     //   available_seats INT
+//     //   )`
+//     // const createBookingTableQuery = `
+//     // create table Booking(
+//     //   id INT AUTO_INCREMENT PRIMARY KEY,
+//     //   seat_number INT,
+//     //   )`
     
-    // const createPaymentTableQuery = `
-    // create table Payment(
-    //   id INT AUTO_INCREMENT PRIMARY KEY,
-    //   amount_paid INT,
-    //   payent_status VARCHAR(20)
-    //   )`
+//     // const createPaymentTableQuery = `
+//     // create table Payment(
+//     //   id INT AUTO_INCREMENT PRIMARY KEY,
+//     //   amount_paid INT,
+//     //   payent_status VARCHAR(20)
+//     //   )`
   
-    connection.execute(createStudentTableQuery, (err, result) => {
-      if (err) {
-        console.error("Error creating table:", err.message);
-        connection.end();
-        return;
-      }
-      console.log("Table created successfully");
-    });
-  });
+//     connection.execute(createStudentTableQuery, (err, result) => {
+//       if (err) {
+//         console.error("Error creating table:", err.message);
+//         connection.end();
+//         return;
+//       }
+//       console.log("Table created successfully");
+//     });
+//   });
 
-  module.exports = connection;
+// module.exports = connection;
